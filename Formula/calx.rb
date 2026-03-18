ffd8e4efcbc66eac0bdd3e7537aa58af9da9c71e
etak <etak64n@gmail.com>
2026-03-18 21:26:37 +0900

    Update calx to v0.5.0

--- Formula/calx.rb
+++ Formula/calx.rb
@@ -1,10 +1,10 @@
 class Calx < Formula
   desc "Native macOS Calendar CLI built on EventKit"
   homepage "https://github.com/etak64n/calx"
-  url "https://github.com/etak64n/calx/releases/download/v0.4.0/calx-v0.4.0-macos-universal.tar.gz"
-  sha256 "ed7fdb5431b21ca82d21b5cc113120a1ee11171abff66f954c15390593dc4510"
+  url "https://github.com/etak64n/calx/releases/download/v0.5.0/calx-v0.5.0-macos-universal.tar.gz"
+  sha256 "a4df6b01057ed0d8026d83402a42534bb3c7968b0a60110fb561c0d67b218c6f"
   license "MIT"
-  version "0.4.0"
+  version "0.5.0"
 
   depends_on :macos
 
